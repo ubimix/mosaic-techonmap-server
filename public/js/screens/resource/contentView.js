a93c9c09b327419a8e49068dc87f121fe9b9d70e
arkub <slauriere@ubimix.com>
2013-10-02 00:10:55 +0200

    - made codemirror instances dynamically configurable - moved Dialog to Utils - udpated pages nav

--- public/js/screens/resource/contentView.js
+++ public/js/screens/resource/contentView.js
@@ -1,17 +1,25 @@
-define([ 'Backbone', '../models/Resource', 'utils', 'text!./contentView.html' ],
+define([ 'Backbone', 'Underscore', '../models/Resource', 'utils', 'text!./contentView.html' ],
 
-function(Backbone, ResourceModel, Utils, ContentViewTemplate) {
+function(Backbone, _, ResourceModel, Utils, ContentViewTemplate) {
+
+    function newCodeMirror(elt, options, readOnly, value) {
+        options = options || {};
+        var defaultOptions = {
+            lineNumbers : false,
+            viewportMargin : Infinity,
+            lineWrapping : true,
+            mode : 'text',
+            readOnly : readOnly
+        };
+        options = _.extend(defaultOptions, options);
+        var editor = new CodeMirror(elt, options);
+        editor.setValue(value);
+        return editor;
+    }
 
     var ResourceContentView = Backbone.View.extend({
         initialize : function(options) {
-//            _.bindAll(this, 'beforeRender', 'render', 'afterRender');
-//            var _this = this;
-//            this.render = _.wrap(this.render, function(render) {
-//                _this.beforeRender();
-//                render();
-//                _this.afterRender();
-//                return _this;
-//            });
+            this.readOnly = this.options.readOnly ? 'nocursor' : false;
         },
         template : _.template(ContentViewTemplate),
         render : function() {
@@ -20,11 +28,16 @@ function(Backbone, ResourceModel, Utils, ContentViewTemplate) {
             });
             this.$el.html(html);
 
+            this.contentEditor = newCodeMirror($('.content').get(0), null, this.readOnly, this.getFormattedContent());
+            this.propertiesEditor = newCodeMirror($('.properties').get(0), {
+                mode : 'yaml'
+            }, this.readOnly, this.getFormattedProperties());
+            $(this.propertiesEditor.getWrapperElement()).addClass('properties');
+
             return this;
 
         },
 
-        /* Utility methods called by the template */
         getFormattedProperties : function() {
             // FIXME
             var properties = this.model.attributes;
@@ -43,32 +56,11 @@ function(Backbone, ResourceModel, Utils, ContentViewTemplate) {
         },
 
         updateModel : function() {
-            // var yaml = this.propertiesEditor.getValue();
-            // var description = this.contentEditor.getValue();
-            var yaml = propertiesEditor.getValue();
-            var description = contentEditor.getValue();
+            var yaml = this.propertiesEditor.getValue();
+            var description = this.contentEditor.getValue();
             var json = Utils.toJSON(description, yaml);
             this.model.set('properties', json);
             return this.model;
-        },
-
-        doLayout : function($el) {
-            // TODO: create editors here instead of having them in the template
-
-        },
-        beforeRender : function() {
-        },
-        afterRender : function() {
-            console.log(this.$el.find('.content'));
-//            var contentEditor = CodeMirror.fromTextArea(this.$el.find('.content').get(0), {
-//                lineNumbers : true,
-//                viewportMargin : Infinity,
-//                lineWrapping : true
-//            });
-           
-            
-            
-            
         }
 
     });
