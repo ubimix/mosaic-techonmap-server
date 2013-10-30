451a57d935152b7274d91d91b5a492ea592d89a6
arkub <slauriere@ubimix.com>
2013-10-30 16:10:13 +0100

    - added numa-config - fixed title editing issue

--- public/js/screens/resource/view.js
+++ public/js/screens/resource/view.js
@@ -38,7 +38,7 @@ function(UmxView, BootstrapGrowl, Xeditable, Validator, viewManager, Utils, Reso
                 this.nameElm.text(this.getTitle());
                 var placeholder = elm.data('title')
                 this.nameElm.editable({
-                    showbuttons:false,
+                    showbuttons:true,
                     highlight:false,
                     emptytext: placeholder||'',
                     unsavedclass:null
