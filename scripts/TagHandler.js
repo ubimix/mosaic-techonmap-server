1b8c425e518de6f5c99dff488ee8f63ead947259
arkub <slauriere@gmail.com>
2014-12-15 19:27:43 +0100

    the tag handler was updated

--- scripts/TagHandler.js
+++ scripts/TagHandler.js
@@ -47,6 +47,19 @@ function replaceTag(object, oldTag, newTag, file) {
     }
 }
 
+
+function addTagIfTag(object, ifTag, newTag, file) {
+    if (!object.tags)
+        return;
+    
+    if (object.tags.indexOf(ifTag) >= 0 && object.tags.indexOf(newTag) < 0) {
+        object.tags.push(newTag);
+        var data = toStructuredContent(object);
+        dumpSync(file, data);
+    }
+}
+
+
 var counter = 0;
 var ids = [];
 var prestataires = [];
@@ -74,26 +87,70 @@ Visitor.visit(dataFolder, function(file, directory) {
 
         } else if (object.category == 'Entreprise') {
             
-            replaceTag(object, 'b2b', 'B2B', file);
-            replaceTag(object, 'apps', 'applications mobiles', file);
-            replaceTag(object, 'design', 'webdesign', file);
-            replaceTag(object, 'Design', 'webdesign', file);
-            replaceTag(object, 'conseils', 'conseil', file);
-            replaceTag(object, 'jeu', 'Jeux vidéo', file);
-            replaceTag(object, 'reseausocial', 'réseau social', file);
+//            replaceTag(object, 'b2b', 'B2B', file);
+//            replaceTag(object, 'apps', 'applications mobiles', file);
+//            replaceTag(object, 'design', 'webdesign', file);
+//            replaceTag(object, 'Design', 'webdesign', file);
+//            replaceTag(object, 'conseils', 'conseil', file);
+//            replaceTag(object, 'jeu', 'Jeux vidéo', file);
+//            replaceTag(object, 'reseausocial', 'réseau social', file);
+            replaceTag(object, 'recommerce', 'ecommerce', file);
+            replaceTag(object, 'mcommerce', 'ecommerce', file);
+            replaceTag(object, 'marketplaces', 'marketplace', file);
+            replaceTag(object, 'sante', 'santé', file);
+            replaceTag(object, 'esante', 'santé', file);
+            replaceTag(object, 'silvereconomy', 'silver économie', file);
+            replaceTag(object, 'silvereco', 'silver économie', file);
+            addTagIfTag(object,'silver économie', 'santé', file);
+            
+            replaceTag(object, 'cinema', 'cinéma', file);
+            addTagIfTag(object,'cinéma', 'culture', file);
+            
+            replaceTag(object, 'mobilite', 'mobilité', file);
+            replaceTag(object, 'geolocalisation', 'géolocalisation', file);
+            addTagIfTag(object,'géolocalisation', 'mobilité', file);
+            
+            replaceTag(object, 'greent it', 'environnement', file);
+            replaceTag(object, 'developpementdurable', 'environnement', file);
+
+            replaceTag(object, 'voyage', 'tourisme', file);
+            replaceTag(object, 'vacances', 'tourisme', file);
+            replaceTag(object, 'bigdata', 'big data', file);
+            replaceTag(object, 'telecommunications', 'telecoms', file);
+            replaceTag(object, 'ingénierie', 'SSII', file);
+            replaceTag(object, 'ingenierie', 'SSII', file);
+            replaceTag(object, 'cloudcomputing', 'cloud', file);
+            
         } else if (object.category == 'École') {
             
-            replaceTag(object, 'designer', 'webdesign', file);
-            replaceTag(object, 'Designer', 'webdesign', file);
-            replaceTag(object, 'graphiste', 'webdesign', file);
-            replaceTag(object, 'Graphiste', 'webdesign', file);
+//            replaceTag(object, 'designer', 'webdesign', file);
+//            replaceTag(object, 'Designer', 'webdesign', file);
+//            replaceTag(object, 'graphiste', 'webdesign', file);
+//            replaceTag(object, 'Graphiste', 'webdesign', file);
+            
+            replaceTag(object, 'ingénieurs', 'ingénieur', file);
+            replaceTag(object, 'programmeur', 'développeur', file);
+            replaceTag(object, 'telecom', 'telecoms', file);
+            
             
         } else if (object.category == 'Tiers-lieu') {
             
-            replaceTag(object, 'bureau', 'bureaux partagés', file);
-            replaceTag(object, 'bureaux', 'bureaux partagés', file);
-            replaceTag(object, 'bureauxpartages', 'bureaux partagés', file);
-            replaceTag(object, 'Graphiste', 'webdesign', file);
+//            replaceTag(object, 'bureau', 'bureaux partagés', file);
+//            replaceTag(object, 'bureaux', 'bureaux partagés', file);
+//            replaceTag(object, 'bureauxpartages', 'bureaux partagés', file);
+//            replaceTag(object, 'Graphiste', 'webdesign', file);
+              replaceTag(object, 'maker', 'makerspace', file);
+              
+        } else {
+            
+            replaceTag(object, 'federation', 'fédération', file);
+            replaceTag(object, 'accélérator', 'accélérateur', file);
+            addTagIfTag(object, 'accompagnement', 'incubateur', file);
+            addTagIfTag(object, 'coaching', 'incubateur', file);
+            
+            replaceTag(object, 'venture capitalist', 'capital risque', file);
+            replaceTag(object, 'venture capital', 'capital risque', file);
+            replaceTag(object, 'greentech', 'cleantech', file);
         }
 
     }
@@ -106,3 +163,4 @@ Visitor.visit(dataFolder, function(file, directory) {
     console.log(prestataires.length);
     console.log('done');
 }).done();
+
