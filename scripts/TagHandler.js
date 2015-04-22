d61082ef32ba3078d05d169f88ef701011aeab41
arkub <slauriere@gmail.com>
2015-04-22 20:59:45 +0200

    tag removals

--- scripts/TagHandler.js
+++ scripts/TagHandler.js
@@ -149,9 +149,11 @@ Visitor.visit(dataFolder, function(file, directory) {
         replaceTag(object, 'vacances', 'tourisme', file);
         replaceTag(object, 'bigdata', 'big data', file);
         replaceTag(object, 'telecommunications', 'telecoms', file);
-        replaceTag(object, 'ingénierie', 'SSII', file);
-        replaceTag(object, 'ingéniérie', 'SSII', file);
-        replaceTag(object, 'ingenierie', 'SSII', file);
+//        replaceTag(object, 'ingénierie', 'SSII', file);
+//        replaceTag(object, 'ingéniérie', 'SSII', file);
+//        replaceTag(object, 'ingenierie', 'SSII', file);
+        
+        replaceTag(object, 'ingénierie ', 'ingénierie', file);
         replaceTag(object, 'cloudcomputing', 'cloud', file);
         
         replaceTag(object, 'economie collaborative', 'économie collaborative', file);
@@ -222,6 +224,7 @@ Visitor.visit(dataFolder, function(file, directory) {
         replaceTag(object, 'energie', 'énergie', file);
         replaceTag(object, 'multimedia', 'multimédia', file);
         replaceTag(object, 'media', 'média', file);
+        replaceTag(object, 'medias', 'média', file);
         replaceTag(object, 'creation', 'création', file);
         replaceTag(object, 'edition', 'édition', file);
         replaceTag(object, 'ingenieur', 'ingénieur', file);
@@ -262,6 +265,125 @@ Visitor.visit(dataFolder, function(file, directory) {
         replaceTag(object, 'objetsconnectes', 'objets connectés', file);
         replaceTag(object, 'conseilregional', 'conseil régional', file);
         replaceTag(object, 'organismeassocie', 'organismes associés', file);
+        
+        replaceTag(object, 'tempsreel', 'temps réel', file);
+        replaceTag(object, 'realiteaugmentee','réalité augmentée', file);
+        replaceTag(object, 'fidelisation','fidélisation', file);
+        replaceTag(object, 'développeur','développeurs', file);
+        
+        replaceTag(object, 'music', 'musique', file);
+        replaceTag(object, 'photographe', 'photo', file);
+        replaceTag(object, 'photographie', 'photo', file);
+        
+        replaceTag(object, 'sports', 'sport', file);
+        replaceTag(object, 'jeux mobiles', 'mobiles', file);
+        
+        replaceTag(object, 'servicedeproximite', 'services de proximité', file);
+        replaceTag(object, 'servicesdeproximite', 'services de proximité', file);
+        
+        replaceTag(object, 'handicapée', 'handicap', file);
+        
+        replaceTag(object, 'multimedias', 'multimédia', file);
+        
+        removeTag(object, 'logiciels informatique', file);
+        removeTag(object, 'reseau', file);
+        removeTag(object, 'lunette', file);
+        removeTag(object, 'pointdevente', file);
+        removeTag(object, '2b2', file);
+        removeTag(object, 'contenus', file);
+        removeTag(object, 'ingéniérie informatique', file);
+        removeTag(object, 'ingénierie informatique', file);
+        removeTag(object, 'achatdurable', file);
+        removeTag(object, 'reingenierie', file);
+        removeTag(object, 'laboratoires', file);
+        removeTag(object, 'usb', file);
+        removeTag(object, 'reingenierie', file);
+        removeTag(object, 'communaute', file);
+        removeTag(object, 'alfresco', file);
+        removeTag(object, 'infrastructures', file);
+        removeTag(object, 'plateforme multeegaming', file);
+        removeTag(object, 'dons', file);
+        removeTag(object, 'multikrens', file);
+        removeTag(object, 'km', file);
+        removeTag(object, 'aventure', file);
+        removeTag(object, 'soa', file);
+        removeTag(object, 'question', file);
+        removeTag(object, 'signal', file);
+        removeTag(object, 'microsoft', file);
+        removeTag(object, 'logiciel informatique', file);
+        removeTag(object, 'echanges', file);
+        removeTag(object, 'participative', file);
+        removeTag(object, 'emulation', file);
+        removeTag(object, 'concertation', file);
+        removeTag(object, 'centreaffaires', file);
+        removeTag(object, 'cabinet', file);
+        removeTag(object, 'films', file);
+        removeTag(object, 'libertes', file);
+        
+        removeTag(object, 'echange entre pro', file);
+        removeTag(object, 'troc 3.0', file);
+        
+        removeTag(object, 'creativetechnologist', file);
+        removeTag(object, 'installations informatiques', file);
+        removeTag(object, 'deveco', file);
+        removeTag(object, 'digitale', file);
+        removeTag(object, 'edition musicale', file);
+        removeTag(object, 'emulationlogicielle', file);
+        removeTag(object, 'doue', file);
+        removeTag(object, 'delegation', file);
+        
+        removeTag(object, 'mecanique', file);
+        removeTag(object, 'symphony', file);
+        removeTag(object, 'multicanal', file);
+        removeTag(object, 'administration', file);
+        removeTag(object, 'entrepreneuriat', file);
+        removeTag(object, 'mecanique', file);
+        removeTag(object, 'nûmerique', file);
+        removeTag(object, 'emotion', file);
+        
+        removeTag(object, 'site', file);
+        removeTag(object, 'collcons', file);
+        removeTag(object, 'identite', file);
+        
+        removeTag(object, 'fildattente', file);
+        
+        removeTag(object, 'serviceclients', file);
+
+        removeTag(object, 'groupe', file);
+        removeTag(object, 'excursions', file);
+        removeTag(object, 'defence', file);
+        
+        removeTag(object, 'géographique', file);
+
+        removeTag(object, 'ingenerie', file);
+        
+        removeTag(object, 'reparation', file);
+        removeTag(object, 'commercants', file);
+        
+        removeTag(object, 'revenuscomplementaires', file);
+        removeTag(object, 'appareilsintelligents', file);
+        removeTag(object, 'inscription', file);
+        removeTag(object, 'siteweb', file);
+        removeTag(object, 'permis de conduire', file);
+        removeTag(object, 'telemedecine', file);
+        removeTag(object, 'pitch', file);
+        removeTag(object, 'utilisateur', file);
+        removeTag(object, 'content', file);
+        removeTag(object, 'pluridisciplainaire', file);
+        removeTag(object, 'commande', file);
+        removeTag(object, 'site de contenu', file);
+        removeTag(object, 'rv', file);
+        removeTag(object, 'websitebuilder', file);
+        removeTag(object, 'anglais', file);
+        removeTag(object, 'travail collaborative', file);
+        removeTag(object, 'programmation informatique.', file);
+        removeTag(object, 'contenus&services', file);
+        removeTag(object, "écialisée dans le secteur d'activité de l'ingénierie", file);
+        
+        
+        
+        
+        
         
     }
 
